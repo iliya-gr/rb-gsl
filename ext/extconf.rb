e429413a26d6d1d569edde839c9bb08c7475b573
Iliya Grushevskiy <iliya.gr@gmail.com>
2013-02-05 12:33:09 +0400

    Refactored native extension build.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,290 +1,98 @@
 require 'mkmf'
 
-
-module GSL
-  class Version
-    def initialize(str)
-      @str = str
-      @ary = str.split(".").collect { |elm| elm.to_i }
-    end
-    def to_s; @str; end
-    def inspect; @str; end
-    def >=(ver)
-      ary2 = ver.split(".").collect { |elm| elm.to_i }
-      if @ary[0] > ary2[0]; return true; end			
-      if @ary[0] < ary2[0]; return false; end
-      if @ary[1] > ary2[1]; return true; end
-      if @ary[1] < ary2[1]; return false; end
-      if @ary.size < ary2.size; return false; end
-      if @ary.size == 3 and ary2.size == 3
-        if @ary[2] < ary2[2]; return false; end
-      end		
-      return true
-    end
-    def <(ver)
-      ary2 = ver.split(".").collect { |elm| elm.to_i }
-      if @ary[0] >= ary2[0]; return false; end
-      if @ary[0] >= ary2[0]; return false; end
-      return true
-    end
-  end
+def gsl_config
+  'gsl-config'
 end
 
-if /mingw/ =~ RUBY_PLATFORM
-  GSL_CONFIG = "sh gsl-config"
+def gsl_version
+  return $gsl_version if $gsl_version
+
+  version = nil
+  IO.popen("#{gsl_config} --version") {|f| version = f.gets.chomp}
+  version ? Gem::Version.new(version) : raise('Ruby/GSL requires gsl-0.9.4 or later.')
+end
+
+def gsl_cflags
+  cflags = nil
+  IO.popen("#{gsl_config} --cflags") {|f| cflags = f.gets.chomp}
+  cflags or raise('Ruby/GSL requires gsl-0.9.4 or later')
+end
+
+def gsl_libs
+  libs = nil
+  IO.popen("#{gsl_config} --libs") {|f| libs = f.gets.chomp}
+  libs or raise('Ruby/GSL requires gsl-0.9.4 or later')
+end
+
+$CFLAGS << " #{gsl_cflags}" 
+
+dir_config("cblas")
+dir_config("atlas")
+
+if have_library("cblas") and have_library("atlas")
+  $LOCAL_LIBS << ' ' + gsl_libs.gsub('-lgslcblas', '-lcblas -latlas')
 else
-  GSL_CONFIG = "gsl-config"
+  $LOCAL_LIBS << ' ' + gsl_libs
 end
 
-def gsl_config()
-  print("checking gsl cflags... ")
-  IO.popen("#{GSL_CONFIG} --cflags") do |f|
-    cflags = f.gets.chomp
-    puts(cflags)
-    $CFLAGS += " " + cflags
-  end
-  
-  IO.popen("#{GSL_CONFIG} --libs") do |f|
-    libs = f.gets.chomp
-    dir_config("cblas")
-    dir_config("atlas")
-    if have_library("cblas") and have_library("atlas")
-      libs.gsub!("-lgslcblas", "-lcblas -latlas")
-      $LOCAL_LIBS += " " + libs.gsub(" -lgslcblas", "")
-      print("checking gsl libs... ")
-      puts(libs)
-    else
-      print("checking gsl libs... ")
-      puts(libs)
-      $LOCAL_LIBS += " " + libs
-    end
-  end
+$defs << '-DGSL_0_9_4_LATER'   if gsl_version >= Gem::Version.new('0.9.4')
+$defs << '-DGSL_1_0_LATER'     if gsl_version >= Gem::Version.new('1.0')
+$defs << '-DGSL_1_1_LATER'     if gsl_version >= Gem::Version.new('1.1')
+$defs << '-DGSL_1_1_1_LATER'   if gsl_version >= Gem::Version.new('1.1.1')
+$defs << '-DGSL_1_2_LATER'     if gsl_version >= Gem::Version.new('1.2')
+$defs << '-DGSL_1_3_LATER'     if gsl_version >= Gem::Version.new('1.3')
+$defs << '-DGSL_1_4_LATER'     if gsl_version >= Gem::Version.new('1.4')
+$defs << '-DGSL_1_4_9_LATER'   if gsl_version >= Gem::Version.new('1.4.9')
+$defs << '-DGSL_1_6_LATER'     if gsl_version >= Gem::Version.new('1.5.9')
+$defs << '-DGSL_1_7_LATER'     if gsl_version >= Gem::Version.new('1.6.9')
+$defs << '-DGSL_1_8_LATER'     if gsl_version >= Gem::Version.new('1.7.9')
+$defs << '-DGSL_1_9_LATER'     if gsl_version >= Gem::Version.new('1.8.9')
+$defs << '-DGSL_1_10_LATER'    if gsl_version >= Gem::Version.new('1.9.9')
+$defs << '-DGSL_1_11_LATER'    if gsl_version >= Gem::Version.new('1.11')
+$defs << '-DGSL_1_13_LATER'    if gsl_version >= Gem::Version.new('1.12.9')
+$defs << '-DGSL_1_14_LATER'    if gsl_version >= Gem::Version.new('1.14')
 
-end
-
-def check_version(configfile)
-  
-  print("checking gsl version... ")
-  IO.popen("#{GSL_CONFIG} --version") do |f|
-    ver = GSL::Version.new(f.gets.chomp)
-    puts(ver)
-    configfile.printf("#ifndef GSL_VERSION\n#define GSL_VERSION \"#{ver}\"\n#endif\n")
-
-    if ver >= "0.9.4"
-      configfile.printf("#ifndef GSL_0_9_4_LATER\n#define GSL_0_9_4_LATER\n#endif\n")
-    else
-      configfile.close
-      raise("Ruby/GSL requires gsl-0.9.4 or later.")
-    end
-    if ver >= "1.0"
-      configfile.printf("#ifndef GSL_1_0_LATER\n#define GSL_1_0_LATER\n#endif\n")
-    end
-    if ver >= "1.1"
-      configfile.printf("#ifndef GSL_1_1_LATER\n#define GSL_1_1_LATER\n#endif\n")
-    end
-    if ver >= "1.1.1"
-      configfile.printf("#ifndef GSL_1_1_1_LATER\n#define GSL_1_1_1_LATER\n#endif\n")
-    end
-    if ver >= "1.2"
-      configfile.printf("#ifndef GSL_1_2_LATER\n#define GSL_1_2_LATER\n#endif\n")
-    end
-    if ver >= "1.3"
-      configfile.printf("#ifndef GSL_1_3_LATER\n#define GSL_1_3_LATER\n#endif\n")
-    end
-    if ver >= "1.4"
-      configfile.printf("#ifndef GSL_1_4_LATER\n#define GSL_1_4_LATER\n#endif\n")
-    end
-    if ver >= "1.4.90"
-      configfile.printf("#ifndef GSL_1_4_9_LATER\n#define GSL_1_4_9_LATER\n#endif\n")
-    end
-    
-    if ver >= "1.5.90"
-      configfile.printf("#ifndef GSL_1_6_LATER\n#define GSL_1_6_LATER\n#endif\n")
-    end
-
-    if ver >= "1.7.90"
-      configfile.printf("#ifndef GSL_1_8_LATER\n#define GSL_1_8_LATER\n#endif\n")
-    end
-    if ver >= "1.8.90"
-      configfile.printf("#ifndef GSL_1_9_LATER\n#define GSL_1_9_LATER\n#endif\n")
-    end
-
-    if ver >= "1.9.90"
-      configfile.printf("#ifndef GSL_1_10_LATER\n#define GSL_1_10_LATER\n#endif\n")
-    end    
-    if ver < "1.4"
-      configfile.printf("#ifndef GSL_CONST_OLD\n#define GSL_CONST_OLD\n#endif\n")
-    end
-
-    if ver >= "1.11"
-      configfile.printf("#ifndef GSL_1_11_LATER\n#define GSL_1_11_LATER\n#endif\n")
-    end    
-
-   if ver >= "1.12.90"
-      configfile.printf("#ifndef GSL_1_13_LATER\n#define GSL_1_13_LATER\n#endif\n")
-    end
-
-    if ver >= "1.14"
-      configfile.printf("#ifndef GSL_1_14_LATER\n#define GSL_1_14_LATER\n#endif\n")
-    end    
-    
-  end
-end
-
-#####
-
-$CFLAGS = " -Wall -I../include "
-
-begin
-  RB_GSL_CONFIG = File.open("../include/rb_gsl_config.h", "w")
-  RB_GSL_CONFIG.printf("#ifndef ___RB_GSL_CONFIG_H___\n")
-  RB_GSL_CONFIG.printf("#define ___RB_GSL_CONFIG_H___\n\n")
-
-  check_version(RB_GSL_CONFIG)
-
-  gsl_config()
-
-  have_func("round")
+# Check functions
+have_func("round")
 
 # Check GSL extensions
-
-  if have_header("rngextra/rngextra.h")
-    have_library("rngextra")
-  end
-
-  if have_header("qrngextra/qrngextra.h")
-    have_library("qrngextra")
-  end
-
-  if have_header("ool/ool_version.h")
-    have_library("ool")
-  end
   
-  if have_header("tensor/tensor.h")
-    have_library("tensor")
-  end
+extensions = {
+  "rngextra/rngextra.h"              => "rngextra",
+  "qrngextra/qrngextra.h"            => "qrngextra",
+  "ool/ool_version.h"                => "ool",
+  "tensor/tensor.h"                  => "tensor",
+  "jacobi.h"                         => "jacobi",
+  "gsl/gsl_cqp.h"                    => "cqp",
+  "gsl/gsl_multimin_fsdf.h"          => "bundle_method",
+  "ndlinear/gsl_multifit_ndlinear.h" => "ndlinear",
+  "alf/alf.h"                        => "alf"
+}
 
-  if have_header("jacobi.h")
-    have_library("jacobi")
-  end
-  if have_header("gsl/gsl_cqp.h")
-    have_library("cqp")
-  end
-  if have_header("gsl/gsl_multimin_fsdf.h")
-    have_library("bundle_method")
-  end
-     
-  if have_library("gsl", "gsl_poly_solve_quartic")
-    RB_GSL_CONFIG.printf("#ifndef HAVE_POLY_SOLVE_QUARTIC\n#define HAVE_POLY_SOLVE_QUARTIC\n#endif\n")
-  end
-  if have_library("gsl", "gsl_eigen_francis")
-    RB_GSL_CONFIG.printf("#ifndef HAVE_EIGEN_FRANCIS\n#define HAVE_EIGEN_FRANCIS\n#endif\n")
-  end
+extensions.each {|header, lib| have_header(header) && have_library(lib)}
 
-  if have_header("ndlinear/gsl_multifit_ndlinear.h")
-    have_library("ndlinear")
-  end
+$defs << '-DHAVE_POLY_SOLVE_QUARTIC' if have_library("gsl", "gsl_poly_solve_quartic")
+$defs << '-DHAVE_EIGEN_FRANCIS'      if have_library("gsl", "gsl_eigen_francis")
 
-# Added 2009/Apr/20
-  if have_header("alf/alf.h")
-    have_library("alf")
-  end
-  
-  begin
-    print("checking rb-gsl version...")
-    IO.popen("cat ../VERSION") do |f|
-      ver = GSL::Version.new(f.gets.chomp)
-      puts(ver)
-      RB_GSL_CONFIG.printf("#ifndef RUBY_GSL_VERSION\n#define RUBY_GSL_VERSION \"#{ver}\"\n#endif\n")
-    end
-  end
+# Ruby version
+RB_VERSION = Gem::Version.new(RUBY_VERSION.dup)
 
-  RUBY_VERSION2 = GSL::Version.new(RUBY_VERSION)
-  
-  puts("checking ruby version... #{RUBY_VERSION2}")
-  if RUBY_VERSION2 >= "1.8"
-    RB_GSL_CONFIG.printf("#ifndef RUBY_1_8_LATER\n#define RUBY_1_8_LATER\n#endif\n")
+$defs << '-DRUBY_1_8_LATER'   if RB_VERSION >= Gem::Version.new('1.8')
+$defs << '-DRUBY_1_9_LATER'   if RB_VERSION >= Gem::Version.new('1.9')
+$defs << '-DRUBY_1_9_2_LATER' if RB_VERSION >= Gem::Version.new('1.9.2')
 
-    if find_executable("graph")
-      RB_GSL_CONFIG.printf("#ifndef HAVE_GNU_GRAPH\n#define HAVE_GNU_GRAPH\n#endif\n")
-    end
-  else
-    path = (path || ENV['PATH']).split(File::PATH_SEPARATOR)  
-    flag = 0
-    print("checking for GNU graph... ")
-    path.each do |dir|    
-      if File.executable?(file = File.join(dir, "graph")) 
-        puts("yes")
-        RB_GSL_CONFIG.printf("#ifndef HAVE_GNU_GRAPH\n#define HAVE_GNU_GRAPH\n#endif\n")
-        flag = 1
-        break
-      end
-    end
-    puts("no") if flag == 0
-  end
-  if RUBY_VERSION2 >= "1.9"
-    RB_GSL_CONFIG.printf("#ifndef RUBY_1_9_LATER\n#define RUBY_1_9_LATER\n#endif\n")
-# Added 2010/Sep/29
-    if RUBY_VERSION2 >= "1.9.2"
-      RB_GSL_CONFIG.printf("#ifndef RUBY_1_9_2_LATER\n#define RUBY_1_9_2_LATER\n#endif\n")
-    end
-  end
+# GNU Graph
+$defs << '-DHAVE_GNU_GRAPH'   if find_executable("graph")
 
-  RB_GSL_CONFIG.printf("\n#endif\n")
-  RB_GSL_CONFIG.close
-  
-rescue
-  raise("Check GSL>=0.9.4 is installed, and the command \"gsl-config\" is in search path.")
+# Find narray with rubygems
+spec = Gem::Specification.find_by_name('narray')
+if spec 
+  $CFLAGS << " -I#{File.join(spec.full_gem_path, spec.require_path)}"
+  have_header('narray.h')
 end
 
-#narray_config = dir_config("narray")
-narray_config = dir_config('narray',$sitearchdir,$sitearchdir)
-# Try to find narray with RubyGems
-begin
-  require 'rubygems'
-  na_gemspec=Gem.searcher.find('narray.h')
-  if na_gemspec
-    narray_config = File.join(na_gemspec.full_gem_path, na_gemspec.require_path)
-    $CPPFLAGS = " -I#{narray_config} "+$CPPFLAGS
-  end
-rescue LoadError
-end
-have_narray_h = have_header("narray.h")
-if narray_config
-  if RUBY_PLATFORM =~ /cygwin|mingw/
-#    have_library("narray") || raise("ERROR: narray import library is not found") 
-  have_library("narray")
-  end
-end
+# Build
+$srcs = Dir[File.join('gsl', "*.{#{SRC_EXT.join(%q{,})}}")] - ["gsl/vector_source.c", "gsl/matrix_source.c", "gsl/tensor_source.c", "gsl/poly_source.c", "gsl/block_source.c"]
 
-tamu_anova_config = dir_config('tamu_anova',$sitearchdir,$sitearchdir)
-have_tamu_anova_h = have_header("tamu_anova/tamu_anova.h")
-if tamu_anova_config
-  have_library("tamuanova")
-#  if RUBY_PLATFORM =~ /cygwin|mingw/
-#    have_library("tamuanova") || raise("ERROR: tamu_anova import library is not found")
-#  end
-end
-
-File.open("../lib/gsl.rb", "w") do |file|
-  if have_narray_h
-    file.print("require('narray')\n")
-  end
-#  file.print("require('rb_gsl')\ninclude GSL\n")
-  file.print("require('rb_gsl')\n")  
-  file.print("require('gsl/oper.rb')\n")
-end
-
-File.open("../lib/rbgsl.rb", "w") do |file|
-  if have_narray_h
-    file.print("require('narray')\n")
-  end
-  file.print("require('rb_gsl')\n")
-  file.print("require('gsl/oper.rb')\n")
-end
-
-srcs = Dir.glob("*.c") - ["vector_source.c", "matrix_source.c", "tensor_source.c", "poly_source.c", "block_source.c"]
-
-$objs = srcs.collect { |f| f.sub(".c", ".o") }
-
-create_makefile("rb_gsl")
+create_header
+create_makefile 'gsl/rb_gsl', 'gsl'
